c7798dbecd4579ba284045853ed8607bbbdfb91f
Braddyers <braddyers@gmail.com>
2025-02-25 21:11:44 +0200

    Edited module: loading data/print ticket

--- module.py
+++ module.py
@@ -7,16 +7,16 @@ from datetime import datetime
 def load_task_data(file_path):
     task_ticket_list = []
     with open(file_path, "r") as file:
-        for index, line in enumerate(file, start = 1):
-            task_data = line.strip().split(", ")               #task_data or line?
+        for task_number, line in enumerate(file, start = 1):
+            task_data = line.strip().split(", ")    
             task_ticket_list.append(task_data)
             
             print(task_ticket_list)
             print(task_data)
-            print(index)
+            print(task_number)
     return task_ticket_list
 
-# Read and convert user_data.txt into a nested list:
+# Read and convert user_data.txt into a nested list THIS IS WRONG: review ticket list?
 def load_user_data(file_path):
     review_ticket_list = []
     with open(file_path, "r") as file:
@@ -42,21 +42,18 @@ def load_personal_data(file_path):
             print(index)
     return personal_data_list
         
-    
-    
-'''
-def print_task_tickets(file_path):
-    for index, task in enumerate(task, start=1):
-        task_data = line.strip().split(", ")               #task_data or line?
-        )
-        print("________________________________________________________" +
-              "________________________________________________________\n")
-        print(f"Ticket number:             {index}")
-        print(f"Assigned to:               {tasked_user}")
-        print(f"Task:                      {task_title}")
-        print(f"Date assigned:             {date_assigned}")
-        print(f"Task due date:             {due_date}")
-        print(f"Task description:        \n{task_description}\n")
-        print("________________________________________________________" +
-              "________________________________________________________\n")
-'''
+def print_task_ticket(file_path) -> str:
+    for task_number, tasked_user, task_title, task_description, date_time_assigned, due_date, task_ticket_number in file_path:
+        return print(
+            "\n________________________________________________________" +
+            "________________________________________________________\n\n" +
+            f"Ticket number:             {task_number}\n" +
+            f"Assigned to:               {tasked_user}\n" +
+            f"Task:                      {task_title}\n" + 
+            f"Date assigned:             {date_time_assigned}\n" + 
+            f"Task due date:             {due_date}\n" + 
+            f"Task description:        \n{task_description}\n" +
+            "________________________________________________________" +
+            f"{task_ticket_number}"
+            "________________________________________________________\n"
+            )    
