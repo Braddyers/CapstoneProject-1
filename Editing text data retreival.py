6642648275a0dee9f0b2f5cf341a58f9fb2b4da2
Braddyers <braddyers@gmail.com>
2025-03-01 13:42:22 +0200

    Edited contents of module in new module file

--- Editing text data retreival.py
+++ Editing text data retreival.py
@@ -1,26 +1,54 @@
+# 1. PERSONAL DATA FUNCTIONS:
+# A - Load personal data method
 personal_data_list = []
-
 with open("personal_data.txt", "r") as file:
     for line in file:
         raw_personal_data = line.strip().split(", ")  # Split each line into a list
         personal_data_list.append(raw_personal_data)  # Append the list to personal_data_list
+        #print(f"1.A \n{personal_data_list}")
 
-print(personal_data_list)
+# B - Load personal data method
+personal_data_list = []
+with open("personal_data.txt", "r") as file:
+    for index, line in enumerate(file, start = 1):
+        raw_personal_data = line.strip().split(", ")  # Split each line into a list
+        personal_data_list.append(raw_personal_data)  # Append the list to personal_data_list
+        #print(f"1.B \n{personal_data_list}")
 
+
+# 2. TASK DATA FUNCTIONS:
+# A - Load task data method
 task_data_list = []
-
 with open("task_data.txt", "r") as file:
     for line in file:
-        raw_task_data = line.strip().split(", ")  # Split each line into a list
-        task_data_list.append(raw_task_data)  # Append the list to personal_data_list
+        raw_task_data = line.strip().split(", ")
+        task_data_list.append(raw_task_data)
+        print(f"2.A \n{task_data_list}")
 
-print(task_data_list)
+# B - Load task data method
+task_data_list = []
+with open("task_data.txt", "r") as file:
+    for index, line in enumerate(file, start = 1):
+        raw_task_data = line.strip().split(", ")
+        task_data_list.append(raw_task_data)
+        print(f"2.B \n{task_data_list}")
 
+# 
+        
+
+# 3. REVIEW TASK DATA FUNCTIONS:
+# A - Load review task data
 review_task_list = []
-
 with open("review_task_data.txt", "r") as file:
     for line in file:
-        raw_review_data = line.strip().split(", ")  # Split each line into a list
-        review_task_list.append(raw_task_data)  # Append the list to personal_data_list
+        raw_review_data = line.strip().split(", ")
+        review_task_list.append(raw_task_data)
+        print(f"3.A \n{review_task_list}")
 
-print(review_task_list)
+# B
+review_task_list = []
+with open("task_data.txt", "r") as file:
+    for index, line in enumerate(file, start = 1):
+        raw_review_task_data = line.strip().split(", ")
+        review_task_list.append(raw_review_task_data)
+        print(f"3.B. \n{review_task_list}")
--- module.py
+++ module.py
@@ -3,44 +3,46 @@ from datetime import datetime
 #=====funtions section=====#
 
 # LOAD DATA FUNCTIONS:
-# Read and convert task_data.txt into a nested list
-def load_task_data(file_path):
-    task_ticket_list = []
+# Read and convert personal_data.txt into a nested list WHICH METHOD BELOW IS MORE USEFUL?
+def load_personal_data(file_path):
+    personal_data_list = []
     with open(file_path, "r") as file:
-        for task_number, line in enumerate(file, start = 1):
-            task_data = line.strip().split(", ")    
-            task_ticket_list.append(task_data)
-            
-            print(task_ticket_list)
-            print(task_data)
-            print(task_number)
-    return task_ticket_list
+        for line in file:
+            raw_personal_data = line.strip().split(", ")  # Split each line into a list
+            personal_data_list.append(raw_personal_data)  # Append the list to personal_data_list
+    return personal_data_list
 
-# Read and convert user_data.txt into a nested list THIS IS WRONG: review ticket list?
-def load_user_data(file_path):
-    review_ticket_list = []
-    with open(file_path, "r") as file:
-        for line in enumerate(file, start = 1):
-            review_data = line.strip().split(", ")
-            review_ticket_list.append(review_data)
-            
-            print(review_ticket_list)
-            print(review_data)
-            print(index)
-    return review_ticket_list
-
-# Read and convert personal_data.txt into a nested list:
 def load_personal_data(file_path):
     personal_data_list = []
     with open(file_path, "r") as file:
         for index, line in enumerate(file, start = 1):
-            personal_data = line.strip().split(", ")
-            personal_data_list.append(personal_data)
+            raw_personal_data = line.strip().split(", ")
+            personal_data_list.append(raw_personal_data)
             
             print(personal_data_list)
-            print(personal_data)
+            print(raw_personal_data)
             print(index)
     return personal_data_list
+
+# Read and convert user_data.txt into a nested list THIS IS WRONG: review ticket list?
+def load_task_data(file_path):
+    task_data_list = []
+    with open(file_path, "r") as file:
+        for line in file:
+            raw_task_data = line.strip().split(", ")  # Split each line into a list
+            task_data_list.append(raw_task_data)  # Append the list to personal_data_list
+    return task_data_list
+
+# Read and convert review task_data.txt into a nested list:
+
+
+def load_review_task_list(file_path):
+    review_task_list = []
+    with open(file_path, "r") as file:
+        for line in file:
+            raw_review_task_data = line.strip().split(", ")  # Split each line into a list
+            review_task_list.append(raw_review_task_data)  # Append the list to personal_data_list
+    return review_task_list
 
 # PRINT TICKET FUNCTION       
 def print_task_ticket(file_path) -> str:
