a7c5a141e329b77a2cbb20ea62ad6e3e7d1ee0be
Braddyers <braddyers@gmail.com>
2025-03-04 16:48:43 +0200

    Edited text data retreival

--- Editing text data retreival.py
+++ Editing text data retreival.py
@@ -10,16 +10,16 @@ Personal_data.txt format:
 personal_data_list = []
 with open("personal_data.txt", "r") as file:
     for line in file:
-        stripped_personal_data = line.strip().split(", ")
+        p = line.strip().split(", ")
         personal_key_values = {
-            "index": int(stripped_personal_data[0]),
-            "username": stripped_personal_data[1],
-            "login_password": stripped_personal_data[2],
-            "first_name": stripped_personal_data[3],
-            "surname": stripped_personal_data[4],
-            "age": stripped_personal_data[5],
-            "date_registered": stripped_personal_data[6],
-            "email_address": stripped_personal_data[7]
+            "index": int(p[0]),
+            "username": p[1],
+            "login_password": p[2],
+            "first_name": p[3],
+            "surname": p[4],
+            "age": p[5],
+            "date_registered": p[6],
+            "email_address": p[7]
         }
         personal_data_list.append(personal_key_values)
 print(f"1.A \n{personal_data_list}\n")
@@ -36,17 +36,17 @@ print(f"1.A \n{personal_data_list}\n")
 task_data_list = []
 with open("task_data.txt", "r") as file:
     for line in file:
-        stripped_task_data = line.strip().split(", ")
-        task_data_key_values = {
-            "index": int(stripped_task_data[0]),
-            "username": stripped_task_data[1],
-            "task_title": stripped_task_data[2],
-            "task_description": stripped_task_data[3],
-            "date_time_assigned": stripped_task_data[4],
-            "task_due_date": stripped_task_data[5],
-            "completed_yes_no": stripped_task_data[6],
+        t = line.strip().split(", ")
+        task_key_values = {
+            "index": int(t[0]),
+            "username": t[1],
+            "task_title": t[2],
+            "task_description": t[3],
+            "date_time_assigned": t[4],
+            "task_due_date": t[5],
+            "completed_yes_no": t[6],
         }
-        task_data_list.append(task_data_key_values)
+        task_data_list.append(task_key_values)
 print(f"1.B \n{task_data_list}\n")
 
 '''
@@ -60,53 +60,77 @@ print(f"1.B \n{task_data_list}\n")
 review_task_list = []
 with open("task_data.txt", "r") as file:
     for line in file:
-        stripped_review_data = line.strip().split(", ")
-        review_data_key_values = {
-            "index": int(stripped_review_data[0]),
-            "username": stripped_review_data[1],
-            "task_due_date": stripped_review_data[2],
-            "task_sub_time": stripped_review_data[3],
+        r = line.strip().split(", ")
+        review_key_values = {
+            "index": int(r[0]),
+            "username": r[1],
+            "task_due_date": r[2],
+            "task_sub_time": r[3],
         }
-        review_task_list.append(review_data_key_values)
+        review_task_list.append(review_key_values)
 print(f"1.C \n{review_task_list}\n")
 
 # 2 - PRINT TICKET FUNCTIONS:
-# C - Print task ticket (TT) FOR USER
+# A - Print task ticket (TT) FOR USER
 
 task_data_list = []
 with open("task_data.txt", "r") as file:
-    for index, line in enumerate(file, start = 1):
-        raw_task_data = line.strip().split(", ")
-        task_data_list.append(raw_task_data)
-        #print(f"1.B \n{task_data_list}\n")
-'''
-print(
-    "\n_______________________________________________________" +
-    "f"TT{task_ticket_number}"
-    "_________________________________________________________\n\n" +
-    f"Task:                      {task_title}\n" + 
-    f"Date assigned:             {date_time_assigned}\n" + 
-    f"Task due date:             {task_due_date}\n" + 
-    f"Task description:        \n{task_description}\n" +
-    "________________________________________________________" +
-    "________________________________________________________\n"
-    )
-'''
-# B - Print task ticket (TT) FOR ADMIN
-'''
-print(
-    "\n_______________________________________________________" +
-    "f"TT{index}"
-    "_________________________________________________________\n\n" +
-    f"Username:                  {username}\n" +
-    f"Task:                      {task_title}\n" + 
-    f"Date assigned:             {date_time_assigned}\n" + 
-    f"Task due date:             {task_due_date}\n" +
-    "________________________________________________________" +
-    "________________________________________________________\n"
-    )
-'''
+    for line in file:
+        t = line.strip().split(", ")
+        task_key_values = {
+            "index": int(t[0]),
+            "username": t[1],
+            "task_title": t[2],
+            "task_description": t[3],
+            "date_time_assigned": t[4],
+            "task_due_date": t[5],
+            "completed_yes_no": t[6],
+        }
+        task_data_list.append(task_key_values)
 
+for task_ticket in task_data_list:
+        print("\nTask Ticket:" +
+            "\n_______________________________________________________" +
+            f"T T {task_ticket["index"]}"
+            "_________________________________________________________\n\n" +
+            f"Task:                      {task_ticket["task_title"]}\n" + 
+            f"Date assigned:             {task_ticket["date_time_assigned"]}\n" + 
+            f"Task due date:             {task_ticket["task_due_date"]}\n" + 
+            f"Task description:        \n{task_ticket["task_description"]}\n" +
+            "________________________________________________________" +
+            "________________________________________________________\n"
+            )
+'''
+# c - Print task ticket (TT) FOR ADMIN
+task_data_list = []
+with open("task_data.txt", "r") as file:
+    for line in file:
+        t = line.strip().split(", ")
+        task_key_values = {
+            "index": int(t[0]),
+            "username": t[1],
+            "task_title": t[2],
+            "task_description": t[3],
+            "date_time_assigned": t[4],
+            "task_due_date": t[5],
+            "completed_yes_no": t[6],
+        }
+        task_data_list.append(task_key_values)
+
+for review_ticket in task_data_list:
+    print(
+        "2.A \nTask Ticket:\n" +
+        "\n_______________________________________________________" +
+        "f"TT{index}"
+        "_________________________________________________________\n\n" +
+        f"Username:                  {username}\n" +
+        f"Task:                      {task_title}\n" + 
+        f"Date assigned:             {date_time_assigned}\n" + 
+        f"Task due date:             {task_due_date}\n" +
+        "________________________________________________________" +
+        "________________________________________________________\n"
+        )
+'''
 
 # C - Print review ticket (RT) FOR ADMIN
 '''
